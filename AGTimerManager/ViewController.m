5acb2ff87f7823ab1d9a55b297b4e3fdad44b4de
JohnnyB0Y <JohnnyB0Y@MacBook-Pro.local>
2018-11-22 04:39:27 +0800

    优化使用方式。

--- AGTimerManager/ViewController.m
+++ AGTimerManager/ViewController.m
@@ -11,7 +11,6 @@
 
 @interface ViewController ()
 
-
 @property (weak, nonatomic) IBOutlet UILabel *countdownLabel;
 - (IBAction)countdownClick:(UISwitch *)sender;
 
@@ -19,7 +18,10 @@
 - (IBAction)timerClick:(UISwitch *)sender;
 
 /** 测试 nil 时，定时器是否停止 */
-@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) AGTimerManager *testNilTM;
+
+/** 计时器 */
+@property (nonatomic, strong) AGTimerManager *timerManager;
 
 @end
 
@@ -33,36 +35,38 @@
     [super viewDidLoad];
     
     // 测试 nil 时，定时器是否停止
-    self.textView = [[UITextView alloc] init];
-	
-    for (NSInteger i = 0; i<24; i++) {
-		
-		[ag_timerManager(self.textView) ag_startCountdownTimer:24 countdown:^BOOL(NSUInteger surplus) {
-			
-			NSLog(@"----- %@", @(surplus));
-			return YES;
-			
-		} completion:^{
-			
-			NSLog(@"完成倒计时！");
-			
-		}];
-		
+    self.testNilTM = [AGTimerManager new];
+    for (NSInteger i = 0; i<6; i++) {
+        
+        __block NSString *key;
+        key = [self.testNilTM ag_startCountdownTimer:6 countdown:^BOOL(NSUInteger surplus) {
+            
+            NSLog(@"%@ -- %@", key, @(surplus));
+            return YES;
+            
+        } completion:^{
+            
+            NSLog(@"完成倒计时！");
+            
+        }];
+        
     }
 	
-	// 5 秒后，制空 self.textView；倒计时会停止，此时不会调用 completion代码块。正常退出才会调用completion代码块。
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5. * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.textView = nil;
+	// 3秒后，制空 testNilTM；倒计时会停止，此时不会调用 completion代码块。正常退出才会调用completion代码块。
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3. * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.testNilTM = nil;
+        NSLog(@"当 self.testNilTM = nil时，停止 testNilTM 的所有定时器！");
     });
 	
-    // 开始计时器
+    
+    // 开始界面上的计时器
     [self _startTimer];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"——————-----------------");
-	NSLog(@"%@", ag_timerManager(nil));
+	NSLog(@"%@", self.timerManager);
 }
 
 #pragma mark - ---------- Event Methods ----------
@@ -91,7 +95,7 @@
 {
     __weak typeof(self) weakSelf = self;
 	_countdownKey =
-	[ag_timerManager(self) ag_startCountdownTimer:[self _countdownTi] countdown:^BOOL(NSUInteger surplus) {
+	[self.timerManager ag_startCountdownTimer:[self _countdownTi] countdown:^BOOL(NSUInteger surplus) {
 		
 		// ———————————————— 倒计时显示 ——————————————————
 		__strong typeof(weakSelf) strongSelf = weakSelf;
@@ -112,13 +116,13 @@
 
 - (void) _stopCountdownTimer
 {
-    [ag_timerManager(self) ag_stopTimerForKey:_countdownKey];
+    [self.timerManager ag_stopTimerForKey:_countdownKey];
 }
 
 - (void) _startTimer
 {
     __weak typeof(self) weakSelf = self;
-    _timerKey = [ag_timerManager(self) ag_startRepeatTimer:1. repeat:^BOOL{
+    _timerKey = [self.timerManager ag_startRepeatTimer:1. repeat:^BOOL{
         
         // ———————————————— 定时任务调用 ——————————————————
         __strong typeof(weakSelf) strongSelf = weakSelf;
@@ -133,11 +137,17 @@
 
 - (void) _stopTimer
 {
-    [ag_timerManager(self) ag_stopTimerForKey:_timerKey];
+    [self.timerManager ag_stopTimerForKey:_timerKey];
 }
 
 
 #pragma mark - ----------- Getter Methods ----------
-
+- (AGTimerManager *)timerManager
+{
+    if (_timerManager == nil) {
+        _timerManager = [AGTimerManager new];
+    }
+    return _timerManager;
+}
 
 @end
